a2ef774eecf65c5440b39aead79c97aca8ef6a07
Miftaudeen <damilarelamidi@gmail.com>
2018-06-04 16:50:22 +0100

    Bug fixes

--- demo/karate/urls.py
+++ demo/karate/urls.py
@@ -1,11 +1,9 @@
-from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.urls import path, include
+from django.views.generic import TemplateView
+from . import views
 
-from karate import views
-
-urlpatterns = patterns('',
-    url(r'^$', direct_to_template, { 'template': 'karate.html'}, name='karate-home'),
-    url(r'^swingtime/events/type/([^/]+)/$', views.event_type, name='karate-event'),
-    (r'^swingtime/', include('swingtime.urls')),
-)
-
+urlpatterns = ['',
+               path(r'^$', TemplateView, {'template': 'karate.html'}, name='karate-home'),
+               path(r'^swingtime/events/type/([^/]+)/$', views.event_type, name='karate-event'),
+               path(r'^swingtime/', include('swingtime.urls')),
+               ]
